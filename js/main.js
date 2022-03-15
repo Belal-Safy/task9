9a1449a718ad7a63097704e4edc25ddd66bc662f
Belal Safy <81881113+Belal-Safy@users.noreply.github.com>
2022-03-15 23:11:26 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+document.querySelector("#btn55").addEventListener("click", () => {
+    document.querySelector(".navbar-collapse").classList.toggle("show");
+    document.querySelector("#btn55").classList.toggle("collapsed");
+})
